c55e8ca70a15a935ba77ea2b7b1efc8080d09ef0
Michaelbretagne <michael.bretagne@gmail.com>
2017-10-10 15:40:38 -0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,26 +29,34 @@ socket.on('newLocationMessage', function(message) {
 
 $('#message-form').on('submit', function(e) {
 	e.preventDefault();
+
+	var messageTextbox = $('[name=message]');
+
 	socket.emit('createMessage', {
 		from: 'User',
-		text: $('[name=message]').val()
+		text: messageTextbox.val()
 	}, function() {
-
+		messageTextbox.val('');
 	});
 });
 
 var locationButton = $('#send-location');
 locationButton.on('click', function() {
-	if (!navigator.geolaction) {
+	if (!navigator.geolocation) {
 		return alert('Geolocation not supported by your browser.');
 	}
 
-	navigator.geolaction.getCurrentPosition(function(position) {
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+	navigator.geolocation.getCurrentPosition(function(position) {
+		locationButton.removeAttr('disabled').text('Send location');
+
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	}, function() {
+		locationButton.removeAttr('disabled').text('Send location');
 		alert('Unable to fetch location.');
 	});
 });
